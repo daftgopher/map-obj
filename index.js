8c4c7859d6a48a34492a5b822412770b41f53f40
daftgopher <mattwaldron@gmail.com>
2017-02-03 15:31:14 -0500

    Convert to ES5

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // customized for this use-case
-const isObject = x =>
-	typeof x === 'object' &&
-	x !== null &&
-	!(x instanceof RegExp) &&
-	!(x instanceof Error) &&
-	!(x instanceof Date);
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var isObject = function isObject(x) {
+	return (typeof x === 'undefined' ? 'undefined' : _typeof(x)) === 'object' && x !== null && !(x instanceof RegExp) && !(x instanceof Error) && !(x instanceof Date);
+};
 
 module.exports = function mapObj(obj, fn, opts, seen) {
 	opts = Object.assign({
@@ -22,23 +22,46 @@ module.exports = function mapObj(obj, fn, opts, seen) {
 
 	seen.set(obj, opts.target);
 
-	const target = opts.target;
+	var target = opts.target;
 	delete opts.target;
 
-	for (const key of Object.keys(obj)) {
-		const val = obj[key];
-		const res = fn(key, val, obj);
-		let newVal = res[1];
+	var _iteratorNormalCompletion = true;
+	var _didIteratorError = false;
+	var _iteratorError = undefined;
 
-		if (opts.deep && isObject(newVal)) {
-			if (Array.isArray(newVal)) {
-				newVal = newVal.map(x => isObject(x) ? mapObj(x, fn, opts, seen) : x);
-			} else {
-				newVal = mapObj(newVal, fn, opts, seen);
+	try {
+		for (var _iterator = Object.keys(obj)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+			var key = _step.value;
+
+			var val = obj[key];
+			var res = fn(key, val, obj);
+			var newVal = res[1];
+
+			if (opts.deep && isObject(newVal)) {
+				if (Array.isArray(newVal)) {
+					newVal = newVal.map(function (x) {
+						return isObject(x) ? mapObj(x, fn, opts, seen) : x;
+					});
+				} else {
+					newVal = mapObj(newVal, fn, opts, seen);
+				}
+			}
+
+			target[res[0]] = newVal;
+		}
+	} catch (err) {
+		_didIteratorError = true;
+		_iteratorError = err;
+	} finally {
+		try {
+			if (!_iteratorNormalCompletion && _iterator.return) {
+				_iterator.return();
+			}
+		} finally {
+			if (_didIteratorError) {
+				throw _iteratorError;
 			}
 		}
-
-		target[res[0]] = newVal;
 	}
 
 	return target;
